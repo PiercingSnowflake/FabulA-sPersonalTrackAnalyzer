2833e27286d8b36f7344f9391a12add5e0bae63d
FabulA <56087824+PiercingSnowflake@users.noreply.github.com>
2023-12-10 11:52:14 +0300

    still doesn't do much. can calculate how many days of your life you have wasted actively listening on spotify

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import json
 
 json_files = ['Data_Filter\\Filtered_Songs.json']  # Use double backslashes or a raw string
-
+total_seconds=0
+dayLength = 86400
 # Iterate through each JSON file
 for json_File in json_files:
     with open(json_File, "r", encoding="utf-8") as file:
         data = json.load(file)
         for json_Object in data:
             the_Json_Object = json_Object
-            print(the_Json_Object)
+            total_seconds += the_Json_Object["ms_played"]/1000
+            #print(the_Json_Object)
+
+print(total_seconds/dayLength)
