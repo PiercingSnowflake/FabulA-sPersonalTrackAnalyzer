a42ce0ecf7980cdd6bece8984b0b163aa549651a
FabulA <56087824+PiercingSnowflake@users.noreply.github.com>
2023-12-11 13:18:59 +0300

    fixed a typo

--- Data_Filter/Data_Filter.py
+++ Data_Filter/Data_Filter.py
@@ -1,7 +1,7 @@
 import json
 
 Json_File_List = ["Streaming_History_Audio_2018-2019_0.json",
-                 - "Streaming_History_Audio_2019_1.json",
+                  "Streaming_History_Audio_2019_1.json",
                   "Streaming_History_Audio_2019-2020_2.json",
                   "Streaming_History_Audio_2020-2021_3.json",
                   "Streaming_History_Audio_2021-2022_4.json",
@@ -23,11 +23,11 @@ for Json_File in Json_File_List:
     with open(Json_File, "r", encoding = "utf-8") as file:
         data = json.load(file)
         for json_object in data:
-            if 'ms_played' in json_object and json_object['ms_played'] > 45000:
-
+            if 'ms_played' in json_object and json_object['ms_played'] > 45000 and \
+                    json_object['master_metadata_track_name'] is not None and \
+                    json_object['spotify_track_uri'] is not None:
                 Extracted_Data = {key: json_object[key] for key in Keys_to_Extract if key in json_object}
                 filtered_entries.append(Extracted_Data)
-
 
 with open("Filtered_Songs.json", "w", encoding="utf-8") as output_file:
     json.dump(filtered_entries, output_file, indent=2)
